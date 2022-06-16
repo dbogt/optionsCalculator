9db1a85c16b4cc1433efa41fcd6dcfcf779a6f98
Bogdan Tudose <59750436+dbogt@users.noreply.github.com>
2022-06-16 18:58:55 -0400

    chart title

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import yfinUDFs as yf
 import pandas as pd
 import plotly.express as px
 
+
 #%% Options Functions
 def N(x):
     return si.norm.cdf(x)
@@ -85,17 +86,19 @@ with colInputs:
     st.metric("{} Last Price".format(ticker),"{:.2f}".format(price))
     st.metric("{} LTM Dividend Yield".format(ticker),"{:.2%}".format(ltmDivYield))
 
+chartTitle = "{} Option Prices at various Strikes (Maturity: {})".format(ticker, expDate)
+
 with colOutputs:
     metricPlot = st.selectbox("Pick a metric to plot:", ('lastPrice','bid','ask'), index=0)
-    figAll = px.scatter(df_all, x='strike', y=metricPlot, color='Type', title="Option Prices at various Strikes")
+    figAll = px.scatter(df_all, x='strike', y=metricPlot, color='Type', title=chartTitle)
     figAll.add_vline(x=price, annotation_text="Current Price: ${:.2f}".format(price))
     st.plotly_chart(figAll)
 
 #fig = px.scatter(df, x='strike', y='lastPrice', title="Last Price at various Strikes for {:%Y-%m-%d}".format(expDate))
-figCalls = px.scatter(df_calls, x='strike', y=['lastPrice','bid','ask'], title="Option Prices at various Strikes")
+figCalls = px.scatter(df_calls, x='strike', y=['lastPrice','bid','ask'], title=chartTitle)
 figCalls.add_vline(x=price, annotation_text="Current Price: ${:.2f}".format(price))
 
-figPuts = px.scatter(df_puts, x='strike', y=['lastPrice','bid','ask'], title="Option Prices at various Strikes")
+figPuts = px.scatter(df_puts, x='strike', y=['lastPrice','bid','ask'], title=chartTitle)
 figPuts.add_vline(x=price, annotation_text="Current Price: ${:.2f}".format(price))
 
 
